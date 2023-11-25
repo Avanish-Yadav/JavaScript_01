bb88b3115ab5bd96592b7fe667133e2dd51468ee
Avanish-Yadav <avanishyadav1906@gmail.com>
2023-11-25 20:30:48 +0530

    callback

---
+++ 05_JS.js
@@ -0,0 +1,12 @@
+setTimeout(function() {
+    console.log("Avanish");
+}, 1000);
+
+setTimeout(function() {
+    console.log("Nimish");
+}, 1000);
+setTimeout(function() {
+    console.log("Harshit");
+}, 1000);
+
+console.log("hello world");
