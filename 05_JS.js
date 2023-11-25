0ba0f0962df48019ea115f1996f50485294c88a5
Avanish-Yadav <avanishyadav1906@gmail.com>
2023-11-25 20:43:53 +0530

    Pyramid of doom

--- 05_JS.js
+++ 05_JS.js
@@ -1,14 +1,20 @@
-setTimeout(function() {
-    console.log("Avanish");
-    setTimeout(function() {
-        console.log("Nimish");
-        setTimeout(function() {
+const p1=new Promise( (res,rej)=>{
+    setTimeout(()=>{
+        console.log("Avanish");
+    },1000)
+})
+
+p1.then(()=>{
+    const p2=new Promise((res,rej)=>{
+        setTimeout(()=>{
+            console.log("Nimish");
+        })
+    })
+    p2.then(()=>{
+        setTimeout(() => {
             console.log("Harshit");
         }, 1000);
-    }, 1000);
-}, 1000);
-
-
-
+    })
+})
 
 console.log("hello world");
