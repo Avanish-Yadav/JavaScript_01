a2ccdd1186f5656cfd224439e3f3cf67c6b9b554
Avanish-Yadav <avanishyadav1906@gmail.com>
2023-11-25 20:32:24 +0530

    Callback hell

--- 05_JS.js
+++ 05_JS.js
@@ -1,12 +1,14 @@
 setTimeout(function() {
     console.log("Avanish");
+    setTimeout(function() {
+        console.log("Nimish");
+        setTimeout(function() {
+            console.log("Harshit");
+        }, 1000);
+    }, 1000);
 }, 1000);
 
-setTimeout(function() {
-    console.log("Nimish");
-}, 1000);
-setTimeout(function() {
-    console.log("Harshit");
-}, 1000);
+
+
 
 console.log("hello world");
